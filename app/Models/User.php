797a35f0ca2724b87ad2bbcf49356f79b13ff810
novaardiansyah <novaardiansyah78@gmail.com>
2025-09-04 15:47:44 +0700

    Configuration of the admin panel provider:
    - Add multi-factor authentication feature
    - Enable profile, forgot password, and email verification features
    - Set the panel to be accessible to all
    - Configure the sidebar menu to be collapsible and set the content's max width to full

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,47 +2,86 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Auth\MultiFactor\Email\Contracts\HasEmailAuthentication;
+use Filament\Models\Contracts\FilamentUser;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Filament\Panel;
+use Filament\Auth\MultiFactor\App\Contracts\HasAppAuthentication;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser, MustVerifyEmail, HasAppAuthentication, HasEmailAuthentication
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+  /** @use HasFactory<\Database\Factories\UserFactory> */
+  use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var list<string>
+   */
+  protected $fillable = [
+    'name',
+    'email',
+    'password',
+  ];
+
+  /**
+   * The attributes that should be hidden for serialization.
+   *
+   * @var list<string>
+   */
+  protected $hidden = [
+    'password',
+    'remember_token',
+    'app_authentication_secret',
+  ];
+
+  /**
+   * Get the attributes that should be cast.
+   *
+   * @return array<string, string>
+   */
+  protected function casts(): array
+  {
+    return [
+      'email_verified_at' => 'datetime',
+      'password' => 'hashed',
+      'app_authentication_secret' => 'encrypted',
+      'has_email_authentication' => 'boolean',
     ];
+  }
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+  public function canAccessPanel(Panel $panel): bool
+  {
+    return true;
+  }
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
+  public function getAppAuthenticationSecret(): ?string
+  {
+    return $this->app_authentication_secret;
+  }
+
+  public function saveAppAuthenticationSecret(?string $secret): void
+  {
+    $this->app_authentication_secret = $secret;
+    $this->save();
+  }
+
+  public function getAppAuthenticationHolderName(): string
+  {
+    return $this->email;
+  }
+
+  public function hasEmailAuthentication(): bool
+  {
+    return $this->has_email_authentication;
+  }
+
+  public function toggleEmailAuthentication(bool $condition): void
+  {
+    $this->has_email_authentication = $condition;
+    $this->save();
+  }
 }
