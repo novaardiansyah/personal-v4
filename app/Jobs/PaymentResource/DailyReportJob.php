22d3a336a9c5659a2e20de337c6a16d5ba7875b4
novaardiansyah <novaardiansyah78@gmail.com>
2025-09-21 21:50:59 +0700

    Added scheduled job to send daily financial reports to email

---
+++ app/Jobs/PaymentResource/DailyReportJob.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Jobs\PaymentResource;
+
+use App\Mail\PaymentResource\DailyReportMail;
+use App\Models\Payment;
+use App\Models\PaymentAccount;
+use App\Services\PaymentService;
+use Carbon\Carbon;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Support\Facades\Mail;
+
+class DailyReportJob implements ShouldQueue
+{
+  use Queueable;
+
+  /**
+   * Create a new job instance.
+   */
+  public function __construct()
+  {
+    //
+  }
+
+  /**
+   * Execute the job.
+   */
+  public function handle(): void
+  {
+    \Log::info('4256 --> DailyReportJob: Started.');
+
+    $startDate = Carbon::now()->startOfWeek();
+    $endDate   = Carbon::now()->endOfWeek();
+    $now       = Carbon::now()->toDateTimeString();
+    $today     = Carbon::now()->toDateString();
+
+    $send = [
+      'filename'   => 'daily-payment-report',
+      'title'      => 'Laporan keuangan harian',
+      'start_date' => $startDate,
+      'end_date'   => $endDate,
+      'now'        => $now,
+    ];
+
+    $pdf = PaymentService::make_pdf($send);
+
+    $payment = Payment::selectRaw('
+      SUM(CASE WHEN type_id = 1 AND date = ? THEN amount ELSE 0 END) AS daily_expense,
+      SUM(CASE WHEN type_id = 2 AND date = ? THEN amount ELSE 0 END) AS daily_income,
+      SUM(CASE WHEN type_id != 1 AND type_id != 2 AND date = ? THEN amount ELSE 0 END) AS daily_other,
+      COUNT(CASE WHEN type_id = 1 AND date = ? THEN id END) AS daily_expense_count,
+      COUNT(CASE WHEN type_id = 2 AND date = ? THEN id END) AS daily_income_count,
+      COUNT(CASE WHEN type_id != 1 AND type_id != 2 AND date = ? THEN id END) AS daily_other_count
+    ', [
+      $today, $today, $today, 
+      $today, $today, $today
+    ])->first();
+
+    $data = [
+      'log_name'         => 'daily_payment_notification',
+      'email'            => getSetting('daily_payment_email'),
+      'author_name'      => getSetting('author_name'),
+      'subject'          => 'Notifikasi: Ringkasan Laporan Keuangan Harian',
+      'payment_accounts' => PaymentAccount::orderBy('deposit', 'desc')->get()->toArray(),
+      'payment'          => $payment->toArray(),
+      'date'             => carbonTranslatedFormat($now, 'd F Y'),
+      'created_at'       => $now,
+      'attachments' => [
+        storage_path('app/' . $pdf['filepath']),
+      ],
+    ];
+
+    Mail::to($data['email'])->queue(new DailyReportMail($data));
+
+    \Log::info('4256 --> DailyReportJob: Finished.');
+  }
+}
--- routes/console.php
+++ routes/console.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Jobs\FileResource\RemoveFileJob;
+use App\Jobs\PaymentResource\DailyReportJob;
 use App\Jobs\PaymentResource\ScheduledPaymentJob;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
@@ -13,6 +14,10 @@ Artisan::command('inspire', function () {
 // ! Scheduled Payment
 Schedule::job(new ScheduledPaymentJob())
   ->dailyAt('00:05');
+
+// ! Daily Payment Report
+Schedule::job(new DailyReportJob())
+  ->dailyAt('23:59');
 
 // ! Scheduled File Deletion
 Schedule::job(new RemoveFileJob())
