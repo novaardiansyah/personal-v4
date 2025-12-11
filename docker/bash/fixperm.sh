81b9040cab84f35f833a2e685bede46453d7ac9d
novaardiansyah <novaardiansyah78@gmail.com>
2025-12-12 01:07:14 +0700

    update permission

--- docker/bash/fixperm.sh
+++ docker/bash/fixperm.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Set owner
-sudo chown -R www:www .
+sudo chown -R www:www . 2>/dev/null
 
 # Default perms
-sudo find . -type d -exec chmod 755 {} \;
-sudo find . -type f -exec chmod 644 {} \;
+sudo find . -type d -exec chmod 755 {} \; 2>/dev/null
+sudo find . -type f -exec chmod 644 {} \; 2>/dev/null
 
 # Writable directories (Laravel + public)
-sudo chmod -R 775 public storage bootstrap/cache vendor
+sudo chmod -R 775 public storage bootstrap/cache vendor 2>/dev/null
 
 # Sensitive files (abaikan error jika tidak ada)
-sudo chmod 600 .env .env-local .env-production artisan .git 2>/dev/null
+sudo chmod 600 .env .env.local .env.production artisan .well-known .git 2>/dev/null
 
-sudo chmod 600 ./docker/bash/fixperm.sh
+sudo chmod 600 ./docker/bash/fixperm.sh 2>/dev/null
