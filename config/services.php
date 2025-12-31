65cd1c4d71673b067a68802820bedacc8dd97ea4
novaardiansyah <novaardiansyah78@gmail.com>
2026-01-01 00:03:13 +0700

    feat: Add `config/services.php` to centralize third-party service configurations.

--- config/services.php
+++ config/services.php
@@ -48,7 +48,7 @@ return [
 
   'self' => [
     'short_url_token' => env('SHORTURL_TOKEN'),
-    'cdn_api_url'     => env('CDN_URL') . '/api',
+    'cdn_api_url'     => env('CDN_API_URL'),
     'cdn_api_key'     => env('CDN_API_KEY'),
     'cdn_url'         => env('CDN_URL'),
   ],
