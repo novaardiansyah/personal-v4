9ca1e910a79019351f446f1f9127ac2cb436224e
novaardiansyah <novaardiansyah78@gmail.com>
2026-01-25 22:56:08 +0700

    feat(mail): update WhatsApp contact details
    - Update WhatsApp phone number and link in contact reply email

--- resources/views/contact-message-resource/mails/reply-contact-mail.blade.php
+++ resources/views/contact-message-resource/mails/reply-contact-mail.blade.php
@@ -9,11 +9,14 @@
 @endsection
 
 @section('content')
-  <p>Terima kasih sudah menghubungi saya. Pesanmu sudah saya terima dan akan saya balas secepatnya, biasanya kurang dari 24 jam.</p>
+  <p>Terima kasih sudah menghubungi saya. Pesanmu sudah saya terima dan akan saya balas secepatnya, biasanya kurang dari
+    24 jam.</p>
 
   <p>Kalau ada pertanyaan lain atau ingin membahas hal lain, silakan hubungi saya lagi kapan saja.</p>
 
   <p>Sekali lagi, terima kasih atas ketertarikanmu. Semoga kita bisa segera ngobrol atau bekerja sama.</p>
 
-  <p>Agar lebih cepat, kamu juga bisa langsung menghubungi saya lewat <a href="https://wa.me/6289506668480?text=Hai+Nova%2C+saya+ingin+terhubung+dengan+Anda+segera%2C+mohon+balas+secepatnya%21" target="_blank">WhatsApp</a>.</p>
+  <p>Agar lebih cepat, kamu juga bisa langsung menghubungi saya lewat <a
+      href="https://wa.me/6282261111084?text=Hai+Nova%2C+saya+ingin+terhubung+dengan+Anda+segera%2C+mohon+balas+secepatnya%21"
+      target="_blank">WhatsApp</a>.</p>
 @endsection
