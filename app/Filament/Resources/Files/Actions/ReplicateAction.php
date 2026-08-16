525cba83c53888e179ba555e8bafa440193f6280
novaardiansyah <novaardiansyah78@gmail.com>
2026-08-17 01:38:28 +0700

    feat(file): replace old uid in file_name and file_path during replicate
    - Replace occurrences of old uid with newly generated uid in file_name
    - Replace occurrences of old uid with newly generated uid in file_path

--- app/Filament/Resources/Files/Actions/ReplicateAction.php
+++ app/Filament/Resources/Files/Actions/ReplicateAction.php
@@ -22,8 +22,21 @@ class ReplicateAction
       ->modalHeading('Replicate File')
       ->modalDescription('Are you sure you want to replicate this file record?')
       ->action(function (File $record, Action $action): void {
-        $replica             = $record->replicate();
-        $replica->uid        = uuid7();
+        $oldUid  = $record->uid;
+        $newUid  = uuid7();
+        $replica = $record->replicate();
+
+        if (!empty($oldUid)) {
+          if ($replica->file_name && str_contains(strtolower($replica->file_name), strtolower($oldUid))) {
+            $replica->file_name = str_ireplace($oldUid, $newUid, $replica->file_name);
+          }
+
+          if ($replica->file_path && str_contains(strtolower($replica->file_path), strtolower($oldUid))) {
+            $replica->file_path = str_ireplace($oldUid, $newUid, $replica->file_path);
+          }
+        }
+
+        $replica->uid        = $newUid;
         $replica->file_alias = ($record->file_alias ?: $record->file_name) . ' (copy)';
         $replica->save();
 
