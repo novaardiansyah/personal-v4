11cc0c7b04fd4239dee147121e26288348ef72d4
novaardiansyah <novaardiansyah78@gmail.com>
2026-09-15 13:12:32 +0700

    feat: add scheduled application jobs and commands in console routes

--- routes/console.php
+++ routes/console.php
@@ -56,7 +56,7 @@ Schedule::job(new SubscriptionReminderJob())
 
 // ! Clean Excess Backups
 Schedule::job(new CleanExcessBackupsJob())
-  ->everyTwoHours();
+  ->everyFifteenMinutes();
 
 // ! Send Pending Discord Messages
 Schedule::job(new SendPendingDiscordMessagesJob())
